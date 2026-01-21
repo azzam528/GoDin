412be579854739153dcf8b675227737e32b4e7fe
Azzam <azzamabdulloh09@gmail.com>
2026-01-22 00:59:50 +0700

    commit kesekian kalinya

--- app/src/main/java/com/example/test/DetailList.kt
+++ app/src/main/java/com/example/test/DetailList.kt
@@ -5,12 +5,44 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import android.widget.TextView
+import android.content.Intent
+import android.widget.ImageView
 class DetailList : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_detail_list)
 
+        val namaMenu = intent.getStringExtra("nama_menu")
+
+        val txtNamaMenu = findViewById<TextView>(R.id.txtNamaMenu)
+        txtNamaMenu.text = namaMenu
+
+        val btnMinus = findViewById<TextView>(R.id.btnMinus)
+        val btnPlus = findViewById<TextView>(R.id.btnPlus)
+        val txtJumlah = findViewById<TextView>(R.id.txtJumlah)
+
+        var jumlah = txtJumlah.text.toString().toInt()
+
+        btnPlus.setOnClickListener {
+            jumlah++
+            txtJumlah.text = jumlah.toString()
+        }
+
+        btnMinus.setOnClickListener {
+            if (jumlah > 1) {   // biar ga minus
+                jumlah--
+                txtJumlah.text = jumlah.toString()
+            }
+        }
+
+        val btnBack = findViewById<ImageView>(R.id.btnBack)
+
+        btnBack.setOnClickListener {
+            val intent = Intent(this, MenuList::class.java)
+            startActivity(intent)
+            finish() // supaya ga numpuk activity
+        }
     }
 }
--- app/src/main/java/com/example/test/MenuList.kt
+++ app/src/main/java/com/example/test/MenuList.kt
@@ -1,16 +1,52 @@
 package com.example.test
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.cardview.widget.CardView
 
 class MenuList : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu_list)
 
+        // ================= BACK KE HOME =================
+        /*val btnBack = findViewById<LinearLayout>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // ================= LOKASI RESTORAN (MAPS) =================
+        val txtLokasi = findViewById<TextView>(R.id.txtLokasi)
+        txtLokasi.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }*/
+
+        // ================= CARD VIEW KE DETAIL =================
+        val cardMieGacoan = findViewById<CardView>(R.id.cardMieGacoan)
+        val cardMieHompimpa = findViewById<CardView>(R.id.cardMieHompimpa)
+
+
+        cardMieGacoan.setOnClickListener {
+            bukaDetail("Mie Gacoan")
+        }
+
+        cardMieHompimpa.setOnClickListener {
+            bukaDetail("Mie Hompimpa")
+        }
+
+
+    }
+
+    private fun bukaDetail(namaMenu: String) {
+        val intent = Intent(this, DetailList::class.java)
+        intent.putExtra("nama_menu", namaMenu)
+        startActivity(intent)
     }
 }
