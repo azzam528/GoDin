0d656123832a87d46b9b029c1c79e95de969e6b7
Azzam <azzamabdulloh09@gmail.com>
2026-01-21 20:23:34 +0700

    first commit

---
+++ app/src/main/java/com/example/test/MenuList.kt
@@ -0,0 +1,16 @@
+package com.example.test
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu_list)
+
+    }
+}
