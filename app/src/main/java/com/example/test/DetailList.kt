96f7257ca140755c6846778c3be0ab5fcb0180f2
fawrellls <rasyenoctavian22@gmail.com>
2026-05-10 21:59:02 +0700

    Implement detail view layout dan data binding

--- app/src/main/java/com/example/test/DetailList.kt
+++ app/src/main/java/com/example/test/DetailList.kt
@@ -1,16 +1,30 @@
 package com.example.test
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class DetailList : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail_list)
 
+        // Menghubungkan TextView dari XML
+        val txtNamaMenu = findViewById<TextView>(R.id.txtNamaMenu)
+        val txtHarga = findViewById<TextView>(R.id.txtHarga)
+        val txtDeskripsi = findViewById<TextView>(R.id.txtDeskripsi)
+
+        // Mengambil data dari Intent
+        val nama = intent.getStringExtra("nama")
+        val harga = intent.getStringExtra("harga")
+        val deskripsi = intent.getStringExtra("deskripsi")
+
+        // Menampilkan data ke halaman detail
+        txtNamaMenu.text = nama
+        txtHarga.text = harga
+        txtDeskripsi.text = deskripsi
     }
 }
